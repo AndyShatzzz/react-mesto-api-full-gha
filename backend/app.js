e4e624c3b56396aa3a4bead637255a7c53b6c531
Andrey Shatskikh <ponchic-09@mail.ru>
2023-09-02 17:07:50 +0300

    feat: Первая заливка

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const defaultErrorNotFound = require('./middlewares/defaultErrorNotFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
